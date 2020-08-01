8fee3269fcc60d44caa09b41bd1885150e770aa1
Ethan Wams <infoptwo@gmail.com>
2020-08-01 17:10:41 +0300

    Completed intro to props

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,16 +1,71 @@
+const players = [
+    {
+        name: "Guil",
+        score: 50,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 85,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 95,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 80,
+        id: 4
+      }    
+]
+
 /**
  * React components must begin with a uppercase to differentiate custom components from normal DOM tags
  */
-function Header(){
-    return (
-        <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
-        </header>
-    )
-}
+const Header = (props) => (
+    <header>
+        <h1>{ props.title }</h1>
+        <span className="stats">Players: { props.totalPlayers }</span>
+    </header>
+)
+
+const Player = (props) => (
+    <div className="player">
+        <span className="player-name">
+            { props.name }
+        </span>
+        <Counter score={ props.score }/>
+    </div>
+)
+
+const Counter = (props) => (
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ props.score }</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+)
+
+const App = (props) => (
+    <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
+
+        {/* Players list */}
+        {
+            props.initialPlayers.map( player =>
+                <Player
+                    name={ player.name }
+                    score={ player.score }
+                    key={ player.id.toString() }
+                />   
+            )
+        }
+    </div>
+)
 
 ReactDOM.render(
-    <Header />,
+    <App initialPlayers={ players } />,
     document.getElementById('root')
 )
